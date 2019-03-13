08332bc928084d64f2c923809eeddbdfe0503adb
Emmanuel KWENE <njume48@gmail.com>
2019-03-13 23:20:04 +0100

    :volcano: Feature: add touch events

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ function Drag(options) {
   this.listener = options.listener || window;
   this.multiplier = options.multiplier || 1;
 
+  // Detect interaction type.
+  this.hasMouseEvent = 'onmousedown' in document;
+  this.hasTouchEvent = 'ontouchstart' in document;
+	this.hasTouchWinEvent = navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1;
+  this.hasPointerEvent = !!window.navigator.msPointerEnabled;
+  this.isTouch = this.hasTouchEvent || this.hasTouchWinEvent || this.hasPointerEvent;
+
+  // Context binding.
   this._onDragStart = this._onDragStart.bind(this);
   this._onDrag = this._onDrag.bind(this);
   this._onDragEnd = this._onDragEnd.bind(this);
@@ -43,16 +51,15 @@ Drag.prototype.off = function(f) {
 };
 
 Drag.prototype._notify = function(e) {
-  var targetX = (e.pageX - this._event.initialX) * this.multiplier;
-  var targetY = (e.pageY - this._event.initialY) * this.multiplier;
+  var _e = (this.isTouch && e.targetTouches) ? e.targetTouches[0] : e;
 
-  this._event.X = targetX;
-  this._event.Y = targetY;
+  this._event.X = (_e.pageX - this._event.initialX) * this.multiplier;
+  this._event.Y = (_e.pageY - this._event.initialY) * this.multiplier;
   this._event.originalEvent = e;
   this._event.cursorDown = this.curDown;
 
-  this._event.initialX = e.pageX;
-  this._event.initialY = e.pageY;
+  this._event.initialX = _e.pageX;
+  this._event.initialY = _e.pageY;
 
   for(var i = 0; i < this.numListeners; i++) {
     this.listeners[i](this._event);
@@ -68,8 +75,10 @@ Drag.prototype._onDrag = function(e) {
 };
 
 Drag.prototype._onDragStart = function(e) {
-  this._event.initialX = e.pageX;
-  this._event.initialY = e.pageY;
+  var _e = (this.isTouch && e.targetTouches) ? e.targetTouches[0] : e;
+  this._event.initialX = _e.pageX;
+  this._event.initialY = _e.pageY;
+
   this.curDown = true;
   //this._notify(e);
 };
@@ -84,17 +93,51 @@ Drag.prototype._onMouseLeave = function(e) {
 };
 
 Drag.prototype._addListeners = function() {
-  this.listener.addEventListener('mouseup', this._onDragEnd);
-  this.listener.addEventListener('mousedown', this._onDragStart);
-  this.listener.addEventListener('mousemove', this._onDrag);
-  this.listener.addEventListener('mouseleave', this._onMouseLeave);
+
+  if (this.hasMouseEvent) {
+    this.listener.addEventListener('mouseleave', this._onMouseLeave);
+    this.listener.addEventListener('mouseup', this._onDragEnd);
+    this.listener.addEventListener('mousedown', this._onDragStart);
+    this.listener.addEventListener('mousemove', this._onDrag);
+  }
+
+  if (this.hasTouchEvent) {
+    this.listener.addEventListener('touchmove', this._onDrag);
+    this.listener.addEventListener('touchend', this._onDragEnd);
+    this.listener.addEventListener('touchstart', this._onDragStart);
+  }
+
+  if (this.hasPointerEvent && this.hasTouchWinEvent) {
+    this.listener.style.msTouchAction = "none";
+
+    this.listener.addEventListener('MSPointerMove', this._onDrag);
+    this.listener.addEventListener('MSPointerUp', this._onDragEnd);
+    this.listener.addEventListener('MSPointerDown', this._onDragStart);
+  }
+
 };
 
 Drag.prototype._removeListeners = function() {
-  this.listener.removeEventListener('mousemove', this._onDrag);
-  this.listener.removeEventListener('mousedown', this._onDragStart);
-  this.listener.removeEventListener('mouseup', this._onDragEnd);
-  this.listener.removeEventListener('mouseleave', this._onMouseLeave);
+
+  if (this.hasMouseEvent) {
+    this.listener.removeEventListener('mouseleave', this._onMouseLeave);
+    this.listener.removeEventListener('mouseup', this._onDragEnd);
+    this.listener.removeEventListener('mousedown', this._onDragStart);
+    this.listener.removeEventListener('mousemove', this._onDrag);
+  }
+
+  if (this.hasTouchEvent) {
+    this.listener.removeEventListener('touchmove', this._onDrag);
+    this.listener.removeEventListener('touchend', this._onDragEnd);
+    this.listener.removeEventListener('touchstart', this._onDragStart);
+  }
+
+  if (this.hasPointerEvent && this.hasTouchWinEvent) {
+    this.listener.removeEventListener('MSPointerMove', this._onDrag);
+    this.listener.removeEventListener('MSPointerUp', this._onDragEnd);
+    this.listener.removeEventListener('MSPointerDown', this._onDragStart);
+  }
+
 };
 
 module.exports = Drag;
